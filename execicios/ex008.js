014dfb91fbd26305855b8a010d1bdc70084604e6
bruno abraao ribeiro rocha <148291732+brunoabraao@users.noreply.github.com>
2024-02-01 10:59:18 -0300

    condição javascript

---
+++ execicios/ex008.js
@@ -0,0 +1,3 @@
+const vel = 60.5 
+console.log('a velocidade so seu carro e ${vel}km/h')
+console.log('voce ultrapasso a velocidade permitida multado ')
---
+++ execicios/ex009.js
@@ -0,0 +1,7 @@
+const pais = 'EUA'
+console.log('vivendo em  ${pais} ' )
+if (pais == 'Brasil') {
+    console.log('Brasileiro')
+} else{
+    console.log('Estranjeiro')
+}
