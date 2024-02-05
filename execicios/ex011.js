982a035d9d24a8071b1f955ed00269817c8075f6
bruno abraao ribeiro rocha <148291732+brunoabraao@users.noreply.github.com>
2024-02-05 11:12:34 -0300

    condiçoes js

---
+++ execicios/ex011.js
@@ -0,0 +1,10 @@
+const idade = 22
+if (idade < 16 ) {
+ console.log('Nao vota ')
+} else {
+    if (idade >= 16 &&  idade < 18 ) { 
+        console.log('voto opicional')  
+} else {
+    console.log('voto obrigatorio')
+}
+ }
---
+++ execicios/ex012.js
@@ -0,0 +1,11 @@
+const agora = new Date
+const horas = agora.getHours
+console.log('Agora são exatamente  ${horas} horas' )
+if(horas < 12) { 
+    console.log('Bom dia ')
+}else if(horas <= 18 ) {
+    console.log('boa tarde ')
+}else{
+    console.log('boa noite')
+}
+
