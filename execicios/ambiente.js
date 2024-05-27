a6989e56667a9315256d79278a216710f673cc63
bruno abraao ribeiro rocha <148291732+brunoabraao@users.noreply.github.com>
2024-05-27 12:51:06 -0300

    java script

---
+++ execicios/ambiente.js
@@ -0,0 +1,5 @@
+let num = [5,8,4 ] 
+console.log(num)
+console.log(`o vetor tem ${num.length} posicoes`)
+console.log(`o primeiro  valor do vetor e ${num[0]}`)
+ 
---
+++ execicios/valores.js
@@ -0,0 +1,5 @@
+let valores = [8,7,9,5,4,1]
+
+for(let pos=0;pos < valores.length; pos++) {
+    console.log(`a posicoes ${pos} tem o valor ${valores[pos]}`)
+}
