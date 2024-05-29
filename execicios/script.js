d35704543eb1b0ab89c319fc5f2714c17346db96
bruno abraao ribeiro rocha <148291732+brunoabraao@users.noreply.github.com>
2024-05-29 15:57:07 -0300

    ex analizando numeros

--- execicios/script.js
+++ execicios/script.js
@@ -1,7 +1,9 @@
-let num = document.querySelector('input#fnum')
-let lista = document.querySelector('selectflista')
-let res = document.querySelector('div#res')
-let valores = []
+const num = document.querySelector('input#fnum')
+const lista = document.getElementById('flista')
+const res = document.querySelector('div#res')
+const valores = []
+const item = document.createElement('option')
+const tot = valores.length
 
 function isNumeros(n){
 if(Number(n) >= 1 && Number(n)<= 100 ){
@@ -9,18 +11,31 @@ if(Number(n) >= 1 && Number(n)<= 100 ){
 } else{
     return false
 }
-}
-function isLista(n,l) {
-if(l.indexof(Number(n)) != -1) {
+  }
+function inLista(n,i) {
+if(i.indexOf(Number(n)) != -1) {
     return true 
 } else{
     return false 
 }
-}
+  }
 function adicionar() {
-    if(isNumeros(num.value) && !isLista (num.value) ) {
-        window.alert('tudo ok')
+    if(isNumeros(num.value) && !inLista (num.value ,valores)) {
+        valores.push(Number(num.value))
+        item.text = ` Valor ${num.value} adicionado `
+        lista.appendChild(item)
     } else {
         window.alert('valor invalido ou ja encontado na lista ')
     }
+}
+num.value = ''
+num.focus()
+
+function finalizar(){
+if(valores.length == 0){
+window.alert (' adiciona valores antes de finalizar')
+} else{
+res.innerHTML = ''
+res.innerHTML += `<p>ao todo temos ${tot} nomeros cadastrados. </p> `
+ }
 }
